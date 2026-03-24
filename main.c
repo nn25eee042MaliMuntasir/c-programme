d57221534d5b2056580978ab1685d5aa57c24351
nn25eee042MaliMuntasir <nn25eee042@nmamit.in>
2026-03-24 10:35:58 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,27 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
+#include <math.h>
 int main()
 {
-    int n,i;
-    char str1[20],str2[20];
-    printf("Enter the word:-");
-    scanf("%s",&str1);
-    n=strlen(str1);
+  int i,n;
+  float avg;
+  printf("enter the number of elements:-");
+  scanf("%d",&n);
+  int sum=0;
+  int a[n];
+  printf("enter the number:");
+  for (i=0;i<n;i++)
+  {
+      scanf("%d",&a[i]);
+      sum=sum+a[i];
+  }
+  avg =(float) sum/n;
+  float var=0;
+  for(i=0;i<n;i++)
+  {
+      var +=((a[i]-avg)*(a[i]-avg)/n);
+  }
+  float st_dev= sqrt(var);
+  printf("the sum of the number is:%d",sum);
+  printf("the average  of the number is:%f",avg);
+  printf("the variance:%f and standard deviationis:%f",var,st_dev);
+  return 0;
+  }
 
-    for (i=0;i<n;i++)
-        {
-            str2[n-i-1]=str1[i];
-        }
-    str2[n]='\0';
-    if (strcmp(str1, str2) ==0)
-       {
-        printf("The entered word is palindrome");
-       }
-    else
-       {
-           printf("The entered word is not palindrome");
-       }
-    return 0;
-}
+
+
+
+
+
+
+
+
+
+
+
+
